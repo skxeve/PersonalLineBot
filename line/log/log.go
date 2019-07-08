bd67c510abeb6df588c0f840746dcd0faeeecaab
skxeve <skxeve@gmail.com>
2019-07-08 23:29:42 +0900

    rename const

--- line/log/log.go
+++ line/log/log.go
@@ -11,11 +11,11 @@ type Logger struct {
 	Context interface{}
 }
 
-const LOG_ENV_GAE = 1
+const LOGGER_ENV_GAE = 1
 
 func (l Logger) Debugf(format string, v ...interface{}) {
 	switch l.Env {
-	case LOG_ENV_GAE:
+	case LOGGER_ENV_GAE:
 		alog.Debugf(l.Context.(context.Context), format, v...)
 	default:
 		log.Printf("[DEBUG] "+format, v...)
@@ -24,7 +24,7 @@ func (l Logger) Debugf(format string, v ...interface{}) {
 
 func (l Logger) Infof(format string, v ...interface{}) {
 	switch l.Env {
-	case LOG_ENV_GAE:
+	case LOGGER_ENV_GAE:
 		alog.Infof(l.Context.(context.Context), format, v...)
 	default:
 		log.Printf("[INFO] "+format, v...)
@@ -33,7 +33,7 @@ func (l Logger) Infof(format string, v ...interface{}) {
 
 func (l Logger) Warningf(format string, v ...interface{}) {
 	switch l.Env {
-	case LOG_ENV_GAE:
+	case LOGGER_ENV_GAE:
 		alog.Warningf(l.Context.(context.Context), format, v...)
 	default:
 		log.Printf("[WARNING] "+format, v...)
@@ -42,7 +42,7 @@ func (l Logger) Warningf(format string, v ...interface{}) {
 
 func (l Logger) Errorf(format string, v ...interface{}) {
 	switch l.Env {
-	case LOG_ENV_GAE:
+	case LOGGER_ENV_GAE:
 		alog.Errorf(l.Context.(context.Context), format, v...)
 	default:
 		log.Printf("[ERROR] "+format, v...)
